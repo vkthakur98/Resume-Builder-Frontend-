2e45c7252046606a98c9030f3222b1247d52597a
Vivek Kumar <vivekthakur1288@gmail.com>
2025-11-05 00:27:45 +0530

    make the resume builder responsive

--- src/components/Builder/HomePage.jsx
+++ src/components/Builder/HomePage.jsx
@@ -16,8 +16,11 @@ const HeroSection = () => ( // Include the TemplateChooser
                 <p className="text-lg text-gray-600 mb-8">
                     Create stunning, professional resumes in minutes with our easy-to-use builder and customizable templates.
                 </p>
+                <p className="block md:hidden lg:hidden text-lg bg-yellow-200 text-red-600 font-semibold py-3 px-8 rounded-xl shadow-xl mb-8">
+                    Please use a desktop or laptop to start building your resume. 
+                </p>
                 
-                <button className="bg-teal-500 text-white font-semibold py-3 px-8 rounded-3xl shadow-xl hover:bg-teal-600 transition duration-300 transform hover:scale-105  cursor-pointer">
+                <button className="hidden md:block lg:block bg-teal-500 text-white font-semibold py-3 px-8 rounded-3xl shadow-xl hover:bg-teal-600 transition duration-300 transform hover:scale-105  cursor-pointer">
                               <Link to="/choosetemplate">Create Your Free Resume</Link> 
                 </button>
             </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div className='header bg-[#FF714C] flex justify-between items-center p-4'>
         <img src={Logo} alt='Resume Builder' height={100} width={100}></img>
         {location.pathname !== "/createresume" && (
-        <button className='bg-[#ffea2df6] text-black font-["Segoe UI", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif] hover:bg-[#ccbc31f6] font-bold px-4 py-2 rounded-3xl cursor-pointer'>
+        <button className='hidden md:block lg:block bg-[#ffea2df6] text-black font-["Segoe UI", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif] hover:bg-[#ccbc31f6] font-bold px-4 py-2 rounded-3xl cursor-pointer'>
           <Link to="/choosetemplate">Create Your Free Resume</Link>
         </button>
       )}   
